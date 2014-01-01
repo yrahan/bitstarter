42241770f45004dfd78f0c509ee7d3bd99271bc6
Yacine Bourkeb <ubuntu@domU-12-31-39-0E-89-9A.compute-1.internal>
2014-01-01 21:33:43 +0000

    Adding index.html and testing Reading file on web.js

--- web.js
+++ web.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+fs.readFile('/home/ubuntu/workspace/bitstarter/index.html', function (err, data) {
+      if (err) throw err;
+        console.log(data);
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
